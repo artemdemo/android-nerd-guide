1c68b9e668e07f1b303136aa190daa8296baac29
Artem Demo <artemdemo@gmail.com>
2015-02-26 00:51:08 +0200

    Added fragments

--- CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/CrimeActivity.java
@@ -1,9 +1,9 @@
 package me.artemdemo.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v4.app.FragmentManager;
 import android.view.MenuItem;
 
 
@@ -12,7 +12,23 @@ public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        /*
+         * fragmentContainer can already exist in the list (for example onCreate will be called after rotation)
+         * Therefore we need to try to find it 
+         */
         setContentView(R.layout.activity_crime);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        
+        // If there is no fragment, then create one
+        if ( fragment == null ) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+            
+        }
     }
 
     @Override
---
+++ CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/CrimeFragment.java
@@ -0,0 +1,46 @@
+package me.artemdemo.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /*
+         * Getting references in Fragment.onCreateView(...) works nearly the same as in
+         * Activity.onCreate(...) . The only difference is that you call View.findViewById(int) on the fragment’s view.
+         */
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        
+        mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle( s.toString() );
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+        
+        return v;
+    }
+}
