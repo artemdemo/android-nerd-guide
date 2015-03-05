f17520e460537253b1a0e080e362ac55edaaa1aa
Artem Demo <artemdemo@gmail.com>
2015-03-05 08:37:30 +0200

    Added language

--- CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/CrimePagerActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends ActionBarActivity {
     // We're adding ViewPager - it will do the trick with swiping right and left
     private ViewPager mViewPager;
     private ArrayList<Crime> mCrimes;
@@ -41,6 +42,22 @@ public class CrimePagerActivity extends FragmentActivity {
             public Fragment getItem(int i) {
                 Crime crime = mCrimes.get(i);
                 return CrimeFragment.newInstance( crime.getId() );
+            }
+        });
+        
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener(){
+            @Override
+            public void onPageScrollStateChanged(int i) {}
+
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {}
+
+            @Override
+            public void onPageSelected(int pos) {
+                Crime crime = mCrimes.get(pos);
+                if ( crime.getTitle() != null ) {
+                    setTitle(crime.getTitle());
+                }
             }
         });
         
--- CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/SingleFragmentActivity.java
+++ CriminalIntent/app/src/main/java/me/artemdemo/criminalintent/SingleFragmentActivity.java
@@ -4,12 +4,13 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 
 /**
  * Using fragments requires activities that know how to manage fragments
  * Basically we can use here "extends Activity", but in order to provide compatible code fro previous android versions I'm using FragmentActivity instead
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends ActionBarActivity {
     /*
      *  We adding abstract method createFragment()
      *  Subclasses of SingleFragmentActivity will implement this method to return an instance of the fragment that the activity is hosting.
